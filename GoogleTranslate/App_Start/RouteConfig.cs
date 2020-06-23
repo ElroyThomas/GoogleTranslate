daed8d10c7fbcb3e3981714373a79ab031629556
Elroy Thomas <elroypt@gmail.com>
2020-06-23 15:26:49 +0530

    adding swagger to project

--- GoogleTranslate/App_Start/RouteConfig.cs
+++ GoogleTranslate/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace GoogleTranslate
             //routes.MapRoute(
             //    name: "Default",
             //    url: "{controller}/{action}/{id}",
-            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            //    defaults: new { controller = "GoogleTranslate", action = "Translate", id = UrlParameter.Optional }
             //);
         }
     }
